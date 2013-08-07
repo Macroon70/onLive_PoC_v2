0ff8fc46102cf577fa0b155e593c56750d224035
vigb <balazs.vig@datasolutions.hu>
2013-08-07 22:51:29 +0200

    2nd video canvas

--- site.js
+++ site.js
@@ -5,6 +5,14 @@ $(function() {
 	console.log(subScreens);
 	subScreens.style.marginTop = mainScreenOffsets.bottom - 15 + "px";
 
+	video = $("#mainvideo")[0];
+	secondcanvas = $("#secondcanvas")[0];
+	secondctx = secondcanvas.getContext('2d');
+	secondctx.rotate(1*Math.PI/180);
+	//secondctx.fillRect(50,20,100,50);
+	
+	copyvideo();
+	
 	window.onscroll = function(event) {
 
 		var subScreenActualOffsets = subScreens.getBoundingClientRect();
@@ -13,3 +21,12 @@ $(function() {
 	}
 
 });
+
+var video;
+var secondcanvas;
+var secondctx;
+
+function copyvideo(){
+  secondctx.drawImage(video, 0, 0);
+  setTimeout("copyvideo();", 1000/25);
+}
