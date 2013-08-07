e5a9b6d75a67ac65389fa0364c627314b88c6f60
vigb <balazs.vig@datasolutions.hu>
2013-08-07 23:50:14 +0200

    video src másolás, de így is dupla letöltés

--- site.js
+++ site.js
@@ -4,6 +4,9 @@ $(function() {
 	var subScreens = document.getElementById('sub_screens');
 	console.log(subScreens);
 	subScreens.style.marginTop = mainScreenOffsets.bottom - 15 + "px";
+	
+	$("#mainvideo").children().clone(false, false).appendTo($("#secondvideo")[0]);
+	//$("#secondvideo")[0].html($("#mainvideo")[0].html());
 
 	window.onscroll = function(event) {
 
